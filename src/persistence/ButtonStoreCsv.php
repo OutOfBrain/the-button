6e978cfb88b8978408a4ad40dc6b1160a3e82849
OutOfBrain <outofbrain@gmail.com>
2015-04-03 13:50:33 +0200

    fix but in getall method for csv

--- src/persistence/ButtonStoreCsv.php
+++ src/persistence/ButtonStoreCsv.php
@@ -6,11 +6,12 @@ namespace Persistence;
 
 class ButtonStoreCsv implements ButtonStoreInterface {
 	private $csvName = 'public/button.csv';
+	private $header = 'now_timestamp,participants,seconds_left';
 
 	public function __construct() {
 		if (!file_exists($this->csvName)) {
 			// init headers
-			file_put_contents($this->csvName, 'now_timestamp,participants,seconds_left' . PHP_EOL);
+			file_put_contents($this->csvName, $this->header . PHP_EOL);
 		}
 	}
 
@@ -19,6 +20,10 @@ class ButtonStoreCsv implements ButtonStoreInterface {
 	}
 
 	public function getAll() {
-		return explode(',', file_get_contents($this->csvName));
+		$lines = explode(PHP_EOL, file_get_contents($this->csvName));
+		foreach ($lines as &$line) {
+			$line = explode(',', $line);
+		}
+		return $lines;
 	}
 }
