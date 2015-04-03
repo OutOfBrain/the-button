b3238efee6a6c2b3e333f844623816ad95be1e81
OutOfBrain <outofbrain@gmail.com>
2015-04-03 12:50:19 +0200

    new csv storage backend

---
+++ src/persistence/ButtonStoreCsv.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Persistence;
+
+
+class ButtonStoreCsv implements ButtonStoreInterface {
+	private $csvName = 'button.csv';
+
+	public function __construct() {
+		if (!file_exists($this->csvName)) {
+			// init headers
+			file_put_contents($this->csvName, 'now_timestamp,participants,seconds_left' . PHP_EOL);
+		}
+	}
+
+	public function insertButton($now_timestamp, $participants, $seconds_left) {
+		file_put_contents($this->csvName, "$now_timestamp,$participants,$seconds_left".PHP_EOL, FILE_APPEND);
+	}
+
+	public function getAll() {
+		return explode(',', file_get_contents($this->csvName));
+	}
+}
