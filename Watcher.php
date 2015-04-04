10067ec4571f2d050ac1d1631d7fd4b8e4767160
OutOfBrain <outofbrain@gmail.com>
2015-04-04 16:56:44 +0200

    adding button watcher - url is instable and i have no way of getting a new one automatically

---
+++ Watcher.php
@@ -0,0 +1,22 @@
+<?php
+
+function mlog($msg) {
+	echo($msg.PHP_EOL);
+}
+mlog('start watching');
+
+$button_was_down = false;
+while (true) {
+	$button_there = `ps aux | grep -v grep | grep ButtonDaemon.php`;
+	var_dump($button_there);
+	if (!$button_there && !$button_was_down) {
+		mlog('no button =( - sending mail');
+		mail('outofbrain@gmail.com', 'Button is down', 'again');
+		$button_was_down = true;
+	} elseif ($button_there && $button_was_down) {
+		// was down and is not anymore
+		mlog('button there agai =)');
+		$button_was_down = true;
+	}
+	sleep(0.5);
+}
